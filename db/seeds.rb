b41552e651b51fbd8cd2754948380cb582c16c49
Harsh Jain <harshagrawaljain@gmail.com>
2018-12-17 07:02:50 -0800

    application is now dockerized

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Post.create(title: 'Post1', text: 'This is Review for Game 1', author_name: 'Harsh Jain')
+Comment.create(text:'This is first comment for Game 1', author_name: 'Reader1', post_id: 1)
+
+
+Post.create(title: 'Post2', text: 'This is Review for Game 2', author_name: 'John Doe')
+Comment.create(text:'This is first comment for Game 2', author_name: 'Reader2', post_id: 2)
